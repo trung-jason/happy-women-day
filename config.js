1dac90fdeae0f869c027399cd4ac83114966103a
trungjason <trungtkt159@gmail.com>
2023-03-08 10:18:08 +0700

    Init

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thanh Huyền';
+const giftUrl = 'https://www.facebook.com/huyen.pt.798';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng em Phạm Thanh Huyền (mập như không mập)';
+const contentCard = 'Chúc em ngày 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'huyen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
